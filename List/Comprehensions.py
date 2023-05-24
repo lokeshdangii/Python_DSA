8018595110442485419dabd81b7aa01f5f08e91b
lokeshdangii <lokeshdangi1045@gmail.com>
2023-05-24 15:42:56 +0530

    Comprehensions in List

---
+++ List/Comprehensions.py
@@ -0,0 +1,23 @@
+l1 = [x for x in range(11) if x%2 == 0]
+print(l1)
+
+l2 = [x for x in range(11) if x%2 != 0]
+print(l2)
+
+l3 = [x for x in [10,24,3,55,32,1,8,2] if x < 10 ]
+print(l3)
+
+l3 = [x for x in [10,24,3,55,32,1,8,2] if x > 10 ]
+print(l3)
+
+
+s = "geeksforgeeks" 
+s1 = [x for x in s if x in "aeiou"]
+print(s1)
+
+s2 = "geeks", "compiler", "assembler", "coureser"
+s3 = [x for x in s2 if x.startswith("c")]
+print(s3)
+
+l4 = [ x*2 for x in range(8)]
+print(l4)
