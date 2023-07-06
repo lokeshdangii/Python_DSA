f849525490fbd1d330b2419276b986802d5c4711
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-06 23:04:50 +0530

    clockwise left rotation of a list by one place

--- List/left_rotate_a_list.py
+++ List/left_rotate_a_list.py
@@ -9,7 +9,29 @@ print(l)
 
 
 # Another Method that is pop append method
-
+'''
 l2 = [2,4,6,8,10]
 l2.append(l2.pop(0))
 print(l2)
+'''
+
+
+
+# Using own Logic
+
+
+def rotateByOne(list1):
+    n = len(list1)
+    first = list1[0]
+
+    for i in range(n-1):
+        list1[i] = list1[i+1]
+
+    list1[n-1] = first
+
+    return list1
+
+
+l1 = [2,4,6,8,10]
+print(rotateByOne(l1))
+
