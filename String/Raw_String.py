3c0e81e6106dae510a759694f61a0d86a59250fe
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-06 23:52:39 +0530

    Raw String (R or r)

---
+++ String/Raw_String.py
@@ -0,0 +1,12 @@
+s1 = "C:\project\name.py" #Here Backslash (\n) will give a new line 
+
+# Raw String --> String where escape sequence don't work i.e Backslash 
+# Can use small or Capital R at the beginning of the string
+
+s2 = r"C:\project\name.py"   
+s3 = R"C:\project\name.py"
+
+
+print(s1)
+print(s2)
+print(s3)
