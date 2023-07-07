368cff07bc53d6e0ceed5c04f71cb35f38054eb0
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-08 01:27:27 +0530

    formatted String

---
+++ String/formatted_string.py
@@ -0,0 +1,13 @@
+name = "ABC"
+course = "Python Course"
+
+
+# using %
+print("Welcome %s to the %s"%(name,course))
+
+# using format() function
+print("Welcome {} to the {}".format(name,course))
+
+
+# using f-string
+print(f"Welcom {name} to the {coursed}")
