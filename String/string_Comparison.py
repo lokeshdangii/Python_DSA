631085ab7bc7b40a6139f0ebcd092af5b9a94de0
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-07 22:44:12 +0530

    Python String Comparison

--- String/string_Comparison.py
+++ String/string_Comparison.py
@@ -8,5 +8,20 @@ print(s1 >= s2)
 print(s1 == s2)
 print(s1 != s2)
 
+print()
+
+print(ord("g"))
+print(ord("i"))
+
+print(ord("G"))
+print(ord("I"))
 
 print(ord("a"))
+
+
+s3 = "python"
+s4 = "Python"
+
+print(s3<s4)
+print(ord("P"))
+print(ord("p"))
