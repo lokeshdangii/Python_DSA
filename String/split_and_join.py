153e5cbbdd85c505057b75e0978daaa832398b33
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-08 14:47:38 +0530

    String split() and join()

---
+++ String/split_and_join.py
@@ -0,0 +1,14 @@
+s1 = "geeks for geeks"
+print(s1.split())
+
+s2 = "geeks, for, geeks"
+print(s2.split(","))
+
+
+l = ["html", "python", "css", "JavaScrip"]
+print(" ".join(l))
+
+print(", ".join(l))
+
+l2 = "geeks for geeks"
+print(",".join(l2))
